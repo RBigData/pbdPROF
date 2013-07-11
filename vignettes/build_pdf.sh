7e5e0257cbbab84b6ef11604b25b469343560153
snoweye <wccsnow@gmail.com>
2013-07-10 22:29:56 -0400

    linking for pbdMPI_1.0-0

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -2,6 +2,8 @@
 
 rm *.aux *.bbl *.blg *.log *.out *.toc
 pdflatex pbdPROF-guide.Rnw
+bibtex pbdPROF-guide
+pdflatex pbdPROF-guide.Rnw
 pdflatex pbdPROF-guide.Rnw
 rm *.aux *.bbl *.blg *.log *.out *.toc *.dvi
 
