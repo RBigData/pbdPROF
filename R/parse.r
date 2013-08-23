908ef5b527c0de190693a946ae3418cd6f617661
G <g@G.(none)>
2013-08-23 16:13:36 +0530

    mpip parser complete

--- R/parse.r
+++ R/parse.r
@@ -81,7 +81,9 @@ parse.prof.mpip(x, ...){
 
 ret_mpip<-list()
 #x<-readLines("mpip.allreduce",n=-1,warn=TRUE)
+#x<-readLines("mpip.allreduce",n=-1,warn=TRUE)
 
+#first part
 #loop flags
 id.start <- grep("@--- MPI Time", x)
 id.end <- grep("@--- Callsites:", x)
@@ -99,9 +101,9 @@ colnames(first_stat)<-c(head)
 #changing datatypes for plotting
 for (j in 1:cloop)
   first_stat[j,] <- copyl[[j]]
+first<-as.data.frame(first_stat)
 
-
-
+##############################################################################
 #second_part
 #loop flags
 id2.start <- grep("@--- Aggregate Time", x)
@@ -123,16 +125,123 @@ head <- as.list(l2[[1]])
 copyl2 <- l2[2:loop] #separate subset for handling the numeric
 cloop2 <- loop-1 #loop limit
 
-second_stat <- matrix(ncol=5,nrow=loop-1,0)
-colnames(second_stat) <- l2[[1]][-1]
-rownames(second_stat) <- c(1:cloop2)
+second_stat <- matrix(ncol=6,nrow=loop-1,0)
+colnames(second_stat) <- l2[[1]]
+#rownames(second_stat) <- c(1:cloop2)
 #changing datatypes for plotting purposes                    
 for (j in 1:cloop2)
-  second_stat[j,] <- as.vector(as.numeric(copyl2[[j]][-1])) #could be [-1] if character are removed
+  second_stat[j,] <- copyl2[[j]] #could be [-1] if character are removed
 
 second <- as.data.frame(second_stat) #converted to data.frame for handling the plotting #second stat
 
-#return list
-ret_mpiP<-list(first=first_stat,second=second_stat)
+
+
+
+#xxxxxxxxxxx----------------------------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-------------------------------xxxxxxxxxxxxxxxxxxxxxxxx  
+#third part of mpiP_profile output
+#x<-readLines("mpip.allreduce",n=-1,warn=TRUE)
+
+id3.start <- grep("@--- Aggregate Sent Message Size", x)
+id3.end <- grep("@--- Callsite Time statistics", x)
+x.sub <- x[(id3.start + 2):(id3.end - 2)]
+loop3<-(id3.end-1)-(id3.start+2)
+
+#the objects starting with "c" are actually modified/copied from orginal objects
+#filtering the 
+l3 <- lapply(seq.int(loop3), function(i) { tmp <- unlist(strsplit(x.sub[i], split=" ")); tmp[which(tmp!="")] })
+head<-as.list(l3[[1]])
+cloop3<-loop3-1
+copyl3<-l3[2:loop3]
+third_stat<-matrix(ncol=6,nrow=loop3-1,0)
+colnames(third_stat)<-c(head)
+#changing datatypes for plotting
+for (j in 1:cloop3)
+  third_stat[j,] <- copyl3[[j]]
+third<-as.data.frame(third_stat)
+
+
+##########################################################
+#fourth stat
+
+
+id4.start <- grep("@--- Callsite Time statistics", x)
+id4.end <- grep("@--- Callsite Message Sent statistics", x)
+x.sub <- x[(id4.start + 2):(id4.end - 2)]
+loop4<-(id4.end-1)-(id4.start+2)
+
+l4 <- lapply(seq.int(loop4), function(i) { tmp <- unlist(strsplit(x.sub[i], split=" ")); tmp[which(tmp!="")] }) #for subsetting the valid #expressions leaving out the spaces
+
+head<-as.list(l4[[1]])
+
+copyl4<-l4[2:loop4]  #separate subset for handling the numeric
+
+
+cloop4<-loop4 - 1  #loop limit
+
+
+adjusted<-loop4+1
+while(adjusted >4){
+  adjusted=adjusted-3
+  l4[[adjusted]]=NULL
+}
+
+fourth_stat<-matrix(ncol=9,nrow=length(l4)-1,0)
+colnames(fourth_stat)<-l4[[1]]
+#rownames(fourth_stat)<-c(1:cloop4-1)
+#removing the empty lines
+
+
+#copying in new object
+copyl4<-l4[2:length(l4)]
+#putting the extracted data into matrixco
+len4=length(l4)-1
+for(j in 1:len4) {
+  #error1: replacement length zero
+  fourth_stat[j,]<-(copyl4[[j]]) } # [-1] if character are removed
+
+fourth_stat[is.na(fourth_stat)]<-0
+fourth<-as.data.frame(fourth_stat) #converted to data.frame for handling the plots
+
+
+##########################################################
+
+id5.start <- grep("@--- Callsite Message Sent statistics", x)
+id5.end <- grep("@--- End of Report --------------------------------------------------------", x)
+x.sub <- x[(id5.start + 2):(id5.end - 2)]
+loop5<-(id5.end-1)-(id5.start+2)
+
+l5 <- lapply(seq.int(loop5), function(i) { tmp <- unlist(strsplit(x.sub[i], split=" ")); tmp[which(tmp!="")] }) #for subsetting the valid #expressions leaving out the spaces
+
+head<-as.list(l5[[1]])
+
+copyl5<-l5[2:loop5]  #separate subset for handling the numeric
+
+
+cloop5<-loop5 - 1  #loop limit
+
+#removing the empty lines
+added<-loop5+1
+
+while(added > 4){
+  added=added-3
+  l5[[added]]=NULL
+}
+fifth_stat<-matrix(ncol=8,nrow=length(l5)-1,0)
+colnames(fifth_stat)<-l5[[1]]
+#rownames(fifth_stat)<-c(1:length(l5)-1)
+
+
+#copying in new object
+copyl5<-l5[2:length(l5)]
+#putting the extracted data into matrixco
+len=length(l5)-1
+for(j in 1:len ){
+  #error1: replacement length zero
+  fifth_stat[j,]<-(copyl5[[j]]) } # [-1] if character are removed
+
+fifth_stat[is.na(fifth_stat)]<-0
+fifth<-as.data.frame(fifth_stat) #converted to data.frame for handling the plots
+
+ret_mpip<-list(first=first,second=second,third=third,fourth=fourth,fifth=fifth)
 return(ret_mpip)
 }
