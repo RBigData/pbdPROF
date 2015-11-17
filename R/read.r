f2ed79aa897b1801b7a302d11f898dac87243315
wrathematics <wrathematics@gmail.com>
2015-11-17 18:56:06 -0500

    add roxygen help to R/read.r

--- R/read.r
+++ R/read.r
@@ -3,16 +3,50 @@ which.profiler <- function(file.name)
 {
   test <- readLines(file.name, n=1)
   
-  if(length(grep(pattern="FPMPI", x=test)) > 0){
+  if(length(grep(pattern="FPMPI", x=test)) > 0)
     return( 'fpmpi' )
-  } else if(length(grep(pattern="mpiP", x=test)) > 0){
+  else if(length(grep(pattern="mpiP", x=test)) > 0)
     return ('mpip')
-  } else{
+  else
     stop("This profiler is not implemented at this time.")
-  }
 }
 
 
+
+#' Reading Profiling Outputs
+#' 
+#' Reader for profiler outputs.
+#' 
+#' This function reads in profiling outputs from MPI-using R code and stores
+#' the output in a \code{prof} class object.  The reading is managed by the
+#' \code{base::readLines()} function.  The user does not need to specify the
+#' type of profiler output being used (e.g., whether the profiler text is from
+#' \pkg{fpmpi}, \pkg{mpiP}, etc.).
+#' 
+#' Additionally, this method automatically parses the output into a condensed,
+#' manageable dataframe (the \code{parsed} slot of the \code{prof} class).
+#' 
+#' @param file.name
+#' a full file name.
+#' @param ... 
+#' options for \code{readLines}
+#' 
+#' @return 
+#' A \code{prof} class object.
+#' 
+#' @examples
+#' \dontrun{
+#' library(pbdPROF)
+#' 
+#' fn <- system.file("data/allreduce.fpmpi", package = "pbdPROF")
+#' da <- read.prof(fn, lib.type = "fpmpi")
+#' 
+#' da
+#' }
+#' 
+#' @seealso \code{ \link{prof-class} }
+#' @keywords utility
+#' @export
 read.prof <- function(file.name, ...)
 {
   if(! file.exists(file.name[1L])){
